4fe5d07deb1c73e794cd96c270f6a78cf9d6d79b
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2016-02-20 03:10:15 +0900

    add notification flow

--- src/components/project/Timer.js
+++ src/components/project/Timer.js
@@ -2,15 +2,46 @@ var React = require('react')
 
 var Timer = React.createClass({
   getInitialState: function() {
-    return null
+    return {
+      time: 0
+    };
   },
 
   componentWillMount: function() {
   },
 
+  tick: function() {
+    this.setState({time: this.state.time - 1});
+
+    if (this.state.time === 0) {
+      var n = new Notification('Time up', {
+        body: 'Let’s have a break.',
+        icon: 'http://www.makky.io/image/favicon-192x192.png',
+      });
+      setTimeout(n.close.bind(n), 10000);
+      clearInterval(this.interval);
+    }
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.setState({time: 10});
+    this.interval = setInterval(this.tick, 1000);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
   render: function() {
     return (
-      <h1>Timer</h1>
+      <div>
+        <h1>Timer</h1>
+        <div>{this.state.time}</div>
+        <form onSubmit={this.handleSubmit}>
+          <button>start</button>
+        </form>
+      </div>
     )
   }
 });
