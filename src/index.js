99edd18cfa033ef6db87f2e0752a354aa5c376aa
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2016-03-11 15:19:06 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-var reactRouter = require('react-router')
-var Router = reactRouter.Router
-var Route = reactRouter.Route
-var Redirect = reactRouter.Redirect
+// Packages
+import React from 'react'
+import {render} from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-var hashHistory = reactRouter.hashHistory
-
-// Page components
-var Layout = require('./components/layout/Default.js')
+// var React = require('react')
+// var ReactDOM = require('react-dom')
+// var reactRouter = require('react-router')
+// var Router = reactRouter.Router
+// var Route = reactRouter.Route
+// var Redirect = reactRouter.Redirect
 //
-// Project components
-var TomatoTimer = require('./components/project/TomatoTimer.js')
-var About = require('./components/project/About.js')
-var TheBank = require('./components/project/TheBank.js')
-
-function onUpdateHandler() {
-  window.scrollTo(0, 0)
-}
-
-onUpdateHandler()
-
-ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="" component={Layout}>
-      <Route path="/tomato-timer" component={TomatoTimer} />
-      <Route path="/the-bank" component={TheBank} />
-      <Route path="/about" component={About} />
-    </Route>
-    <Redirect from="/" to="/tomato-timer" />
-  </Router>
-), document.getElementById('react-root'))
+// var hashHistory = reactRouter.hashHistory
+//
+// // Page components
+// var Layout = require('./components/layout/Default.js')
+// //
+// // Project components
+// var TomatoTimer = require('./components/project/TomatoTimer.js')
+// var About = require('./components/project/About.js')
+// var TheBank = require('./components/project/TheBank.js')
+//
+// function onUpdateHandler() {
+//   window.scrollTo(0, 0)
+// }
+//
+// onUpdateHandler()
+//
+// ReactDOM.render((
+//   <Router history={hashHistory}>
+//     <Route path="" component={Layout}>
+//       <Route path="/tomato-timer" component={TomatoTimer} />
+//       <Route path="/the-bank" component={TheBank} />
+//       <Route path="/about" component={About} />
+//     </Route>
+//     <Redirect from="/" to="/tomato-timer" />
+//   </Router>
+// ), document.getElementById('react-root'))
