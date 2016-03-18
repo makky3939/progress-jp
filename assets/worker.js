65a894dfff4c498e5fd230603e99639957377c8d
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2016-03-18 15:09:09 +0900

    Updates

---
+++ assets/worker.js
@@ -0,0 +1,10 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+onmessage = function(e) {
+  console.log('Message received from main script');
+  console.log(e.data)
+  console.log('Posting message back to main script');
+  postMessage(e.data);
+}
+
+
+},{}]},{},[1])
